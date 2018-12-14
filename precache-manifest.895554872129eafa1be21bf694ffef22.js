bc0e94fc2380758f1af09b59a124692dfa190caf
Ken Crites <kenrcrites@gmail.com>
2018-12-14 11:55:21 +0100

    Updates

---
+++ precache-manifest.895554872129eafa1be21bf694ffef22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d37c9a3806a951dd35",
+    "url": "/starwarsapi/static/js/runtime~main.37d37c9a.js"
+  },
+  {
+    "revision": "98687e0324f3817fb469",
+    "url": "/starwarsapi/static/js/main.98687e03.chunk.js"
+  },
+  {
+    "revision": "d24c17080246af8445d1",
+    "url": "/starwarsapi/static/js/1.d24c1708.chunk.js"
+  },
+  {
+    "revision": "98687e0324f3817fb469",
+    "url": "/starwarsapi/static/css/main.77ac8ffb.chunk.css"
+  },
+  {
+    "revision": "d24c17080246af8445d1",
+    "url": "/starwarsapi/static/css/1.6767b0c6.chunk.css"
+  },
+  {
+    "revision": "9aefc331025e56e4296c7d4114ff3aba",
+    "url": "/starwarsapi/index.html"
+  }
+];
